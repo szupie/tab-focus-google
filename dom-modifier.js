c9037a3f0cf49c0f5e4b19ae05a639c9c0b79dc9
Angus Lo <szupie@gmail.com>
2018-11-11 14:17:02 -0500

    scroll result into center of page

--- dom-modifier.js
+++ dom-modifier.js
@@ -1,6 +1,11 @@
-const results = document.querySelectorAll(".LC20lb");
+const results = document.querySelectorAll('.LC20lb');
 
 for (let i=0; i<results.length; i++) {
 	const linkNode = results[i].closest('a');
-	linkNode.setAttribute("tabindex", 1);
+	linkNode.setAttribute('tabindex', 1);
+	linkNode.addEventListener('focus', centerResult);
+}
+
+function centerResult(e) {
+	e.target.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
