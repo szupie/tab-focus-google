35363c194d6dd8c0364e8c4a55977035967457e6
Angus Lo <szupie@gmail.com>
2018-11-12 00:03:04 -0500

    removed buggy scroll into view behaviour

--- dom-modifier.js
+++ dom-modifier.js
@@ -3,9 +3,4 @@ const results = document.querySelectorAll('.LC20lb');
 for (let i=0; i<results.length; i++) {
 	const linkNode = results[i].closest('a');
 	linkNode.setAttribute('tabindex', 1);
-	linkNode.addEventListener('focus', centerResult);
-}
-
-function centerResult(e) {
-	e.target.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
