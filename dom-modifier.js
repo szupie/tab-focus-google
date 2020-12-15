c4e554b288d4cff3a02616838a168878050686e0
Angus Lo <szupie@gmail.com>
2020-12-15 01:31:16 -0500

    scroll entire result into view when changing focus

--- dom-modifier.js
+++ dom-modifier.js
@@ -4,3 +4,19 @@ for (let i=0; i<results.length; i++) {
 	const linkNode = results[i].closest('a');
 	linkNode.setAttribute('tabindex', 1);
 }
+
+// capture focus changes on results list to scroll entire result into view
+document.getElementById('res').addEventListener("focus", e => {
+	// only perform scroll if newly focused element is result link
+	if (e.target.getAttribute('tabindex') === '1') {
+		const resultNode = e.target.closest('.g');
+		const bounds = resultNode.getBoundingClientRect();
+		// scroll item to top if it extends past viewport top,
+		// or to bottom if it extends past viewport bottom
+		if (bounds.top < 0) {
+			resultNode.scrollIntoView();
+		} else if (bounds.bottom > window.innerHeight) {
+			resultNode.scrollIntoView(false);
+		}
+	}
+}, true);
